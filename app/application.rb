1a0bdfe1ee484d81adaa3705fbb66ac9911067cf
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-03-04 03:46:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,14 +13,14 @@ class Application
 
       if item.nil?
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       else
         resp.write item.price
       end
 
     else
       resp.write "Route not found"
-      resp.status = 400
+      resp.status = 404
     end
 
     resp.finish
