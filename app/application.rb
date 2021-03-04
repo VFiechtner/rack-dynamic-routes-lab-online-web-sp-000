4293d1f6062231f506cb1fd1c2a4a09c7232cc99
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-03-04 03:44:04 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      item = @@items.find{|i| i.name == item_name}
+
+      
